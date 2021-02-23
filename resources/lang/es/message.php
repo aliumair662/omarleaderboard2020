ab5db52b2275fdaee08a6de97479eca7e4dee465
aliumair662 <aliumair662@gmail.com>
2021-02-23 16:00:58 +0500

    add languages fodler

---
+++ resources/lang/es/message.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Greece Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login-giveaway' => 'Iniciarsésion para vernuestro leaderboard sorteoendirecto',
+    'email' => 'Dirección de email',
+    'password' => 'contraseña',
+    'login-button' => 'iniciarsésion',
+    'login-Forgot' => 'has olvidado la contraseña?',
+    'login-to-Sign' => 'crear una cuenta',
+    'login-dont-have-account' => 'No tienes una cuenta?',
+    'already-have-account' => 'Yaestásregistrado ?',
+    'footer-text' => 'El leaderboard es una plataforma de juegosdescentralizada . Los jugadorespuedenganaspremiosenefectivo y artículos de lujojugando los Leaderboard juegos .Nuestrosjuegos son accesiblesentodo el mundo . Los premiosenefectivo se pagarán a través de PayPal y todos los premios que no seanenefectivoseránenviados gratis a sudomicilio .Todos los juegos se basanen la habilidad y no por casualidad . El leaderboard no estáregulado por la comisión de juego .',
+    'sign-giveaway' => 'Creatucuenta para poderver el leaderboard  endirecto ',
+    'firstname' => 'nombre',
+    'lastname' => 'Apellidos',
+    'password_confirmation' => 'confirmacontraseña',
+    'agree' => 'Acepto los términos y condiciones y la  ',
+    'terms&conditions' => 'política de privacidad',
+    'createaccount' => 'Crear una cuenta',
+    'Sign-in' => 'Registrarse',
+    'forgot-password-giveaway' => 'Te has olvidado de tucontraseña no tepreocupestevamos a enviar un enlace enseguida .',
+    'send-password-link' => 'Send Password Reset Link',
+    'instagram-giveaway' => 'envíanostu @nombre eninstagram para que podamosidentificarteen el Leaderboard !',
+    'instagram-username' => 'Instagram Nombre de usuario',
+    'add-instagram-account' => 'Agregarcuenta de Instagram',
+    'no-password-required-instagram' => 'No se requiere contraseña! Su @nombre de Instagram solo se utilizará para vincularlo a su cuenta de Instagram en The Leaderboard.',
+    'exit-game' => 'Salir del juego',
+    'see-prizes' => 'Ver premios',
+    'competitions-end-in' => 'LA COMPETENCIA TERMINA EN',
+    'days' => 'Dias',
+    'hours' => 'Horas',
+    'mins' => 'Minutos',
+    'secs' => 'Segundos',
+    'not-on-the-live-leaderboard' => 'No estás en la tabla de clasificación en vivo, comienza a etiquetar a tus amigos para que aparezcan en la tabla de clasificación en vivo.',
+    'you-are' => 'Usted está',
+    'out-of' => 'fuera de',
+    'players' => 'jugadores',
+    'entry-post' => 'puesto de entrada',
+    'how-it-works' => 'Cómo funciona',
+    'tag-friends-now' => 'Etiquetar amigas ahora',
+    'top-100' => 'Tabla de clasificación - Top 100',
+    'see-post' => 'Ver publicación',
+    'continue-playing' => 'Seguirjugando',
+    'mentions-away-from-position'=>'menciona lejos de la posición'
+
+
+];
---
+++ resources/lang/pt/message.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Greece Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login-giveaway' => 'Faça login para vernossosorteioao vivo do quadro de classificação ',
+    'email' => 'E-mail',
+    'password' => 'Senha',
+    'login-button' => 'iniciarsessão',
+    'login-Forgot' => 'Esqueceu a senha ?',
+    'login-to-Sign' => 'Inscrever-se',
+    'login-dont-have-account' => 'Não tem uma conta?',
+    'already-have-account' => 'Jaestaregistrado?',
+    'footer-text' => 'A tabela de classificação é umaplataforma de jogodescentralizada. Osjogadorespodemganharprêmiosemdinheiro de bens de luxoaojogarosjogos da tabela de classificação. Nossosjogosestãoacessíveisemtodo o mundo. Osprêmiosemdinheiroserãopagos via PayPal e todososprêmios que nãosejamemdinheiroserãopublicadosgratuitamenteemtodo o mundo. Todososjogossãobaseadosnahabilidade e não por acaso. Nãosomosregulamentados pela comissão de jogo.',
+    'sign-giveaway' => 'Primeiro, vamos criar uma conta para que você possa ver o placar ao vivo!',
+    'firstname' => 'Primeiro nome',
+    'lastname' => 'Sobrenome',
+    'password_confirmation' => 'Confirm Password',
+    'agree' => 'Eu Aceite o termo e as',
+    'terms&conditions' => 'condiçôes da publicaçaoprivada',
+    'createaccount' => 'Criar uma conta',
+    'Sign-in' => 'Entrar',
+    'forgot-password-giveaway' => 'Esqueceu a palavra-passe ? Não se precoupeiremosenviarlhe um link o maisrapidopossivel.',
+    'send-password-link' => 'Send Password Reset Link',
+    'instagram-giveaway' => 'Mete o seuinstagram @nome para que entaonospodemosidentificar-te no leaderboard !',
+    'instagram-username' => 'instagram Username',
+    'add-instagram-account' => 'adicione a conta do instagram ',
+    'no-password-required-instagram' => 'No password required! Your Instagram @name will only be used to link you to your Instagram account on The Leaderboard.',
+    'exit-game' => 'Sair do jogo',
+    'see-prizes' => 'Vejaosprêmios',
+    'competitions-end-in' => 'COMPETIÇÕES TERMINA EM',
+    'days' => 'Dias',
+    'hours' => 'Horas',
+    'mins' => 'Mins',
+    'secs' => 'Segundos',
+    'not-on-the-live-leaderboard' => 'Vocênãoestá no placarao vivo, comece a marcarseus amigos para aparecer no placarao vivo.',
+    'you-are' => 'Tu es',
+    'out-of' => 'fora de',
+    'players' => 'jogadoras',
+    'entry-post' => 'Posto de entrada ',
+    'how-it-works' => 'Como funciona',
+    'tag-friends-now' => 'IDENTIFIQUE OS  SEUS AMIGOS AGORA',
+    'top-100' => 'Tabela de classificação - top 100 ',
+    'see-post' => 'Ver Postagem',
+    'continue-playing' => 'Continue jogando',
+    'mentions-away-from-position'=>'menções fora da posição'
+
+];
